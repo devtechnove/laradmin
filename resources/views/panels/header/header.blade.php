c8c32e38a5975e5e7df43e713f18a0742ffaa00e
Devtechve <devtechve@gmail.com>
2022-12-08 16:38:08 -0400

    adecuacio de plantilla vuexy

--- resources/views/panels/header/header.blade.php
+++ resources/views/panels/header/header.blade.php
@@ -403,12 +403,14 @@ id="layout-navbar"
       </ul>
     </li>
     <!--/ Notification -->  --}}
-
+    @php
+       $img_src = 'https://ui-avatars.com/api/?name='.Auth::user()->name;
+    @endphp
     <!-- User -->
     <li class="nav-item navbar-dropdown dropdown-user dropdown">
       <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
         <div class="avatar avatar-online">
-          <img src="../../assets/img/avatars/1.png" alt class="h-auto rounded-circle" />
+          <img src="{{ $img_src }}" alt class="h-auto rounded-circle" />
         </div>
       </a>
       <div class="dropdown-menu dropdown-menu-end">
